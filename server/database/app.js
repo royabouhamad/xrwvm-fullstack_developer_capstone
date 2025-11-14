f7d47292130710253f32d19793903d0f871ef395
Roy Abouhamad <Roy.Abouhamad@trayport.com>
2025-11-14 14:06:38 +0000

    Fix linting errors

--- server/database/app.js
+++ server/database/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+mongoose.connect("mongodb://mongo_db:27017/", {'dbName': 'dealershipsDB'});
 
 
 const Reviews = require('./review');
@@ -19,21 +19,21 @@ const Reviews = require('./review');
 const Dealerships = require('./dealership');
 
 try {
-  Reviews.deleteMany({}).then(()=>{
+  Reviews.deleteMany({}).then(() => {
     Reviews.insertMany(reviews_data['reviews']);
   });
-  Dealerships.deleteMany({}).then(()=>{
+  Dealerships.deleteMany({}).then(() => {
     Dealerships.insertMany(dealerships_data['dealerships']);
   });
   
 } catch (error) {
-  res.status(500).json({ error: 'Error fetching documents' });
+  console.log(error);
 }
 
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -88,9 +88,9 @@ app.get('/fetchDealer/:id', async (req, res) => {
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
-  data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  let data = JSON.parse(req.body);
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,
